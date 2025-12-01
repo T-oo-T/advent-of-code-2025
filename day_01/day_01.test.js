df19dd187739166ebc596b86bc4bc34bae0eb4f9
Otto Peräkylä <otto.perakyla@gmail.com>
2025-12-01 09:00:41 +0200

    init

---
+++ day_01/day_01.test.js
@@ -0,0 +1,10 @@
+const { test } = require("node:test")
+const assert = require("assert").strict
+
+test("foo", () => {
+    assert.deepEqual(1,1)
+})
+
+test("bar", () => {
+    assert.deepEqual(1,1)
+})
