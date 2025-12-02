186d77f4ef278f612970234d6d787cbd4ceb2c4f
Otto Peräkylä <otto.perakyla@gmail.com>
2025-12-02 10:04:24 +0200

    day 2 part 1

--- day_02/day_02.js
+++ day_02/day_02.js
@@ -1,7 +1,20 @@
 const { readFile } = require("../utils")
 
 function part1(filePath) {
-    return 1227775554
+    let ranges = readFile(filePath).split(",")
+    let sum = 0
+    
+    for (let i = 0; i < ranges.length; i++) {
+        let [start, end] = ranges[i].split("-").map(s => parseInt(s, 10))
+        for (let j = start; j <= end; j++) {
+            let jStr = j.toString()
+            if (jStr.length % 2 == 0 && jStr.substring(0, jStr.length / 2) == jStr.substring(jStr.length / 2)) {
+                sum += parseInt(jStr, 10)
+            }
+        }
+    }
+
+    return sum
 }
 
 module.exports = {
--- day_02/day_02.test.js
+++ day_02/day_02.test.js
@@ -2,6 +2,10 @@ const { test } = require("node:test")
 const assert = require("assert").strict
 const { part1  } = require("./day_02")
 
-test("part1", () => {
+test("part1, sample input", () => {
     assert.equal(part1("./input-sample.txt"), 1227775554)
+})
+
+test("part1, real input", () => {
+    assert.equal(part1("./input-real.txt"), 44487518055)
 })
