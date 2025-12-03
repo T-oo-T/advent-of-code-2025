0819d277f579718a14049ec514e8748eee02205d
Otto Peräkylä <otto.perakyla@gmail.com>
2025-12-03 09:13:25 +0200

    part2

--- day_02/day_02.js
+++ day_02/day_02.js
@@ -7,9 +7,9 @@ function part1(filePath) {
     for (let i = 0; i < ranges.length; i++) {
         let [start, end] = ranges[i].split("-").map(s => parseInt(s, 10))
         for (let j = start; j <= end; j++) {
-            let jStr = j.toString()
-            if (jStr.length % 2 == 0 && jStr.substring(0, jStr.length / 2) == jStr.substring(jStr.length / 2)) {
-                sum += parseInt(jStr, 10)
+            let t = j.toString()
+            if (t.length % 2 == 0 && t.substring(0, t.length / 2) == t.substring(t.length / 2)) {
+                sum +=j 
             }
         }
     }
@@ -17,6 +17,49 @@ function part1(filePath) {
     return sum
 }
 
+function part2(filePath) {
+    let ranges = readFile(filePath).split(",")
+    let sum = 0
+    
+    for (let i = 0; i < ranges.length; i++) {
+        let [start, end] = ranges[i].split("-").map(s => parseInt(s, 10))
+        for (let j = start; j <= end; j++) {
+            if (sequenceOfRepeats(j)) {
+                sum+=j
+            }
+        }
+    }
+
+    return sum
+}
+
+function sequenceOfRepeats(number) {
+    let t = number.toString()
+    let n = t.length
+
+    for (let k = 2; k <= n; k++) {
+        if (n % k == 0) {
+            let parts = []
+            let partSize = n/k
+
+            for (let i = 0; i < k; i++) {
+                parts.push(t.substring(i*partSize, i*partSize+partSize))
+            }
+
+            let allPartsEqual = true
+            for (let i = 0; i < parts.length - 1; i++) {
+                if (parts[i] != parts[i+1]) allPartsEqual = false
+            }
+
+            if (allPartsEqual) return true
+        }
+    }
+
+    return false
+}
+
 module.exports = {
-    part1
+    part1,
+    part2,
+    sequenceOfRepeats
 }
--- day_02/day_02.test.js
+++ day_02/day_02.test.js
@@ -1,6 +1,6 @@
 const { test } = require("node:test")
 const assert = require("assert").strict
-const { part1  } = require("./day_02")
+const { part1, part2, sequenceOfRepeats } = require("./day_02")
 
 test("part1, sample input", () => {
     assert.equal(part1("./input-sample.txt"), 1227775554)
@@ -8,4 +8,47 @@ test("part1, sample input", () => {
 
 test("part1, real input", () => {
     assert.equal(part1("./input-real.txt"), 44487518055)
+})
+
+test("sequenceOfRepeats", () => {
+    assert.equal(sequenceOfRepeats(1), false)
+    assert.equal(sequenceOfRepeats(12), false)
+    assert.equal(sequenceOfRepeats(11), true)
+    assert.equal(sequenceOfRepeats(22), true)
+    assert.equal(sequenceOfRepeats(99), true)
+    assert.equal(sequenceOfRepeats(111), true)
+    assert.equal(sequenceOfRepeats(999), true)
+    assert.equal(sequenceOfRepeats(1010), true)
+    assert.equal(sequenceOfRepeats(113), false)
+    assert.equal(sequenceOfRepeats(118118),true)
+    assert.equal(sequenceOfRepeats(1188511885), true)
+    assert.equal(sequenceOfRepeats(1111111), true)
+    assert.equal(sequenceOfRepeats(12341234), true)
+    assert.equal(sequenceOfRepeats(123123123), true)
+    assert.equal(sequenceOfRepeats(1212121212), true)
+    assert.equal(sequenceOfRepeats(12345), false)
+    assert.equal(sequenceOfRepeats(12312355),false)
+    assert.equal(sequenceOfRepeats(222222),true)
+    assert.equal(sequenceOfRepeats(446446),true)
+    assert.equal(sequenceOfRepeats(38593859),true)
+    assert.equal(sequenceOfRepeats(655655),true)
+    assert.equal(sequenceOfRepeats(9957599575),true)
+    assert.equal(sequenceOfRepeats(101), false)
+    assert.equal(sequenceOfRepeats(1001), false)
+    assert.equal(sequenceOfRepeats(1011), false)
+    assert.equal(sequenceOfRepeats(1188511881), false)
+    assert.equal(sequenceOfRepeats(446444), false)
+    assert.equal(sequenceOfRepeats(565655), false)
+    assert.equal(sequenceOfRepeats(2121212122), false)    
+    assert.equal(sequenceOfRepeats(3202320), false)
+    assert.equal(sequenceOfRepeats(3273273),false)
+})
+
+test("part2, sample input", () => {
+    assert.equal(part2("./input-sample.txt"), 4174379265)
+})
+
+
+test("part2, real input", () => {
+    assert.equal(part2("./input-real.txt"), 53481866137)
 })
