5800f1c1399ec5c45d0ef90ec2ca0f2dd6380f78
Otto Peräkylä <otto.perakyla@gmail.com>
2025-12-04 11:22:02 +0200

    split files

---
+++ day_04/day_04.js
@@ -0,0 +1,65 @@
+const { readFile, sum } = require("../utils")
+
+function parseFile(filePath) {
+    return readFile(filePath)
+        .split("\n")
+        .map(row => 
+            row.split("")
+                .map(char => char == "." ? 0 : 1)
+        )
+}
+
+function accessibleRolls(grid) {
+    let rolls = []
+
+    let val = ([i, j]) => grid[i]?.[j] || 0
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] == 0) continue
+            
+            let adjacentCoords = [
+                // previous row
+                [i-1,j-1], [i-1,j], [i-1,j+1],
+                // next row
+                [i+1,j-1], [i+1,j], [i+1,j+1],
+                // same row
+                [i,j-1], [i,j+1]
+            ]
+
+            let adjacentRolls = adjacentCoords.map(val).reduce(sum)
+
+            if (adjacentRolls < 4) rolls.push([i,j])
+        }
+    }
+
+    return rolls
+}
+
+function part1(filePath) {
+    let grid = parseFile(filePath)
+    return accessibleRolls(grid).length
+}
+
+function part2(filePath) {
+    let grid = parseFile(filePath)
+    let totalRollsRemoved = 0
+
+    while (accessibleRolls(grid).length > 0) {
+        let rollsToRemove = accessibleRolls(grid)
+        totalRollsRemoved += rollsToRemove.length
+
+        for (let [i, j] of rollsToRemove) {
+            grid[i][j] = 0
+        }
+    }
+
+    return totalRollsRemoved
+}
+
+module.exports = {
+    parseFile,
+    accessibleRolls,
+    part1,
+    part2
+}
