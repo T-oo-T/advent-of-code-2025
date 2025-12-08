d1d8771e9ad58c6617ce00f840d473c1c73c54de
Otto Peräkylä <otto.perakyla@gmail.com>
2025-12-08 13:43:23 +0200

    part 1

---
+++ day_08/day_08.test.ts
@@ -0,0 +1,54 @@
+// @ts-ignore
+import test from "node:test"
+// @ts-ignore
+import assert from "node:assert/strict"
+import { part1, part2, UnionFind } from "./day_08.ts"
+
+test("part1, sample input", () => {
+    assert.deepStrictEqual(part1("input-sample.txt"), 40)
+})
+
+test("UnionFind", () => {
+    let unionFind = new UnionFind(6)
+    
+    assert.deepStrictEqual(unionFind.components, [0,1,2,3,4,5])
+    
+    assert.equal(unionFind.find(0), 0)
+    assert.equal(unionFind.find(1), 1)
+    assert.equal(unionFind.find(2), 2)
+    assert.equal(unionFind.find(3), 3)
+    assert.equal(unionFind.find(4), 4)
+    assert.equal(unionFind.componentCount(), 6)
+    
+    unionFind.union(0,2)
+    assert.deepStrictEqual(unionFind.components, [2,1,2,3,4,5])
+    assert.equal(unionFind.componentCount(), 5)
+
+    unionFind.union(1,3)
+    assert.deepStrictEqual(unionFind.components, [2,3,2,3,4,5])
+    assert.equal(unionFind.componentCount(), 4)
+
+    unionFind.union(2,1)
+    assert.deepStrictEqual(unionFind.components, [3,3,3,3,4,5])
+    assert.equal(unionFind.componentCount(), 3)
+
+    unionFind.union(5,4)
+    assert.deepStrictEqual(unionFind.components, [3,3,3,3,4,4])
+    assert.equal(unionFind.componentCount(), 2)
+
+    unionFind.union(0,5)
+    assert.deepStrictEqual(unionFind.components, [4,4,4,4,4,4])
+    assert.equal(unionFind.componentCount(), 1)
+})
+
+test("part1, real input", () => {
+    assert.deepStrictEqual(part1("input-real.txt"), 79560)
+})
+
+test.skip("part2, sample input", () => {
+    assert.deepStrictEqual(part2("input-sample.txt"), -1)
+})
+
+test.skip("part2, real input", () => {
+    assert.deepStrictEqual(part2("input-real.txt"), -1)
+})
---
+++ day_08/day_08.ts
@@ -0,0 +1,82 @@
+import { readFile } from "../utils.js"
+
+export function dist([x1,y1,z1]: number[], [x2,y2,z2]: number[]): number {
+    return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2) + Math.pow(z1-z2, 2))
+}
+
+type Edge = {
+    from: number,
+    to: number,
+    weight: number
+}
+
+export class UnionFind {
+    components: number[]
+
+    constructor(n: number) {
+        this.components = Array.from(new Array(n).keys())
+    }
+
+    find(i: number): number {
+        return this.components[i]
+    }
+
+    union(a: number, b: number): undefined {
+        let a_root = this.find(a)
+        let b_root = this.find(b)
+
+        for (let i = 0; i < this.components.length; i++) {
+            if (this.components[i] == a_root) {
+                this.components[i] = b_root
+            }
+        }
+    }
+
+    componentCount(): number {
+        return new Set(this.components).size
+    }
+
+    componentSizes(): number[] {
+        let compSizes = new Array(this.components.length).fill(0)
+
+        for (let i = 0; i < this.components.length; i++) {
+            compSizes[this.find(i)]++
+        }
+
+        return compSizes
+    }
+}
+
+export function part1(filePath: string): number {
+    let junctionBoxes: number[][] = readFile(filePath)
+        .split("\n")
+        .map((row:string) => row.split(",").map((d:string) => Number(d)))
+    
+    let unionFind = new UnionFind(junctionBoxes.length)
+    let connectionCount = filePath == "input-real.txt" ? 1000 : 10
+    let edges: Edge[] = []
+
+    for (let i = 0; i < junctionBoxes.length; i++) {
+        for (let j = i + 1; j < junctionBoxes.length; j++) {
+            edges.push({from: i, to: j, weight: dist(junctionBoxes[i],junctionBoxes[j])})
+        }   
+    }
+    edges.sort((a,b) => a.weight - b.weight)
+    
+    for (let i = 0; i < connectionCount; i++) {
+        let edge = edges[i]    
+        unionFind.union(edge.from,edge.to)
+    }
+    
+    let compSizes = unionFind.componentSizes()
+    
+    compSizes.sort((a,b) => b - a)
+    
+    let [c1,c2,c3] = compSizes
+     
+    return c1 * c2 * c3
+}
+
+export function part2(filePath: string) {
+
+}
