1897b0de1da86fe3c92e58a28d4953a6a08c7a57
Otto Peräkylä <otto.perakyla@gmail.com>
2025-12-08 13:45:39 +0200

    remove export

--- day_08/day_08.ts
+++ day_08/day_08.ts
@@ -1,6 +1,6 @@
 import { readFile } from "../utils.js"
 
-export function dist([x1,y1,z1]: number[], [x2,y2,z2]: number[]): number {
+function dist([x1,y1,z1]: number[], [x2,y2,z2]: number[]): number {
     return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2) + Math.pow(z1-z2, 2))
 }
 
